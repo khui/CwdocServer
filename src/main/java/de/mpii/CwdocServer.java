d20f489083a6cfca705ca4b6f03642a0f593b88e
lhyan792 <khui@mpi-inf.mpg.de>
2016-09-07 22:56:03 +0200

    first commit

---
+++ src/main/java/de/mpii/CwdocServer.java
@@ -0,0 +1,103 @@
+package de.mpii;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ *
+ @author Arunav, Kai
+ */
+public class CwdocServer extends HttpServlet {
+
+    private Map<String, String> request2qidcwid = new HashMap<>();
+    private String root;
+
+    public void init(ServletConfig cfg)throws ServletException{
+        root = cfg.getServletContext().getInitParameter("application.dir");
+        String requestid2qidcwid = root + "/cw4y/dictionary/did2docurl.dict";
+        try(BufferedReader br = new BufferedReader(new FileReader(new File(requestid2qidcwid)))) {
+            while(br.ready()){
+                String line = br.readLine();
+                String[] cols = line.split(" ");
+                request2qidcwid.put(cols[0], cols[1]);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     @param request servlet request
+     @param response servlet response
+     @throws ServletException if a servlet-specific error occurs
+     @throws IOException if an I/O error occurs
+     */
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        String docid = request.getParameter("docid");
+        String qidcwid = request2qidcwid.get(docid);
+        String fileName = root + "/cw4y/" + qidcwid + "/part-00000";
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            while (br.ready()) {
+                sb.append(br.readLine());
+            }
+        }
+        try (PrintWriter out = response.getWriter()) {
+            out.println(sb.toString());
+        }
+    }
+
+    /**
+     Handles the HTTP <code>GET</code> method.
+     @param request servlet request
+     @param response servlet response
+     @throws ServletException if a servlet-specific error occurs
+     @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        processRequest(request, response);
+    }
+
+
+    /**
+     Handles the HTTP <code>POST</code> method.
+     @param request servlet request
+     @param response servlet response
+     @throws ServletException if a servlet-specific error occurs
+     @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    /**
+     Returns a short description of the servlet.
+     @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+
+}
---
+++ src/main/java/de/mpii/ErrorHandler.java
@@ -0,0 +1,51 @@
+package de.mpii;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Copy from http://www.tutorialspoint.com/servlets/servlets-exception-handling.htm
+ * Revised by Kai.
+ */
+
+
+// Extend HttpServlet class
+public class ErrorHandler extends HttpServlet {
+
+    // Method to handle GET method request.
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Sorry, there are something wrong...";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n");
+
+        out.println("<h2>There are some errors with the requested page.</h2>");
+        out.println("<br /><p>Sorry for this inconvenience. Please mark this page as \"The URL is not valid\" and " +
+                "move on to the next judgment.</p>");
+        out.println("<br /><p>^_^</p>");
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+    // Method to handle POST method request.
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
