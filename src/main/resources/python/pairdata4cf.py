6de90937acbece4251c7e510ed795ef938d0c8bf
lhyan792 <khui@mpi-inf.mpg.de>
2016-09-15 00:17:33 +0200

    pairwise data for cf

---
+++ src/main/resources/python/pairdata4cf.py
@@ -0,0 +1,277 @@
+import hadoopy as hy
+from pyspark import SparkContext, SparkConf
+import random, os, math
+import os.path
+import xml.etree.ElementTree as ET
+
+def relgradedqrel(sc, qid, qrelf, parNum=24):
+    cwidlabel= sc.textFile(qrelf, parNum) \
+            .map(lambda line : line.split(' ')) \
+            .filter(lambda cols : len(cols) == 4) \
+            .filter(lambda cols : int(cols[0]) == qid) \
+            .map(lambda cols : (cols[2], int(cols[-1])))\
+            .collect()
+    cwidGrad = dict()
+    # take the largest grade among all subtopics/facets
+    # for diversity qrel
+    for cwid, label in cwidlabel:
+        # remove junk
+        if label < 0:
+            continue
+        if label > 1 and label != 4:
+            label = 2
+        cwidGrad[cwid] = label
+    return cwidGrad
+def readcwidurl(sc, qid, urlfile, parNum=24):
+    cwidurls= sc.textFile(urlfile + "/" + str(qid) + "/part*", parNum) \
+            .map(lambda line : line.split(' ')) \
+            .filter(lambda cols : len(cols) == 2) \
+            .map(lambda cols : (cols[0], cols[-1].replace(",","/")))\
+            .collect()
+    return dict(cwidurls)
+def readDid4ttsa(dictfile):
+    qidCwiddocid = dict()
+    for line in open(dictfile):
+        cols=line.split(' ')
+        qidcwid = cols[1].split('/')
+        q = int(qidcwid[0])
+        if q not in qidCwiddocid:
+            qidCwiddocid[q]=dict()
+        qidCwiddocid[q][qidcwid[1].rstrip()]=cols[0]
+    return qidCwiddocid
+
+def labelDocs(cwidGrad):
+    ldocs=dict()
+    for d, l in list(cwidGrad.items()):
+        if l not in ldocs:
+            ldocs[l]=list()
+        ldocs[l].append(d) 
+    return ldocs
+def readInQuery(queryfile):
+    queries = sc.textFile(queryfile, 1).collect()
+    queries='\n'.join(queries)
+    root = ET.fromstring(queries)
+    qidQuery=dict()
+    for query in root:
+        qid = int(query.attrib['number'])
+        qtype = query.attrib['type']
+        shorttxt = ' '.join(query.findall('query')[0].text.replace('\n', '').\
+                replace(',', ' ').title().split())
+        descrip = ' '.join(query.findall('description')[0].text.replace('\n', '').\
+                replace(',', ' ').split())
+        subqueries = query.findall('subtopic')
+        subquery=dict()
+        for s in subqueries:
+            sid = int(s.attrib['number'])
+            stype = s.attrib['type']
+            sshorttxt = ' '.join(s.text.replace('\n', '').split())
+            subquery[sid]=(stype, sshorttxt)
+        qidQuery[qid]=(qtype, shorttxt, descrip)
+    return qidQuery
+def selectDocs(labelDocs, cwidGrad, judnum=12): 
+    doc4jud=list()
+    doc4test=list()
+    labels = list(labelDocs.keys())
+    docnumperL = judnum / len(labels)
+    for l in labels:
+        cwids = labelDocs[l]
+        if len(cwids) < 3:
+            continue
+        random.shuffle(cwids)
+        doc4jud.extend(cwids[:docnumperL])
+    allcwids = list(cwidGrad.keys())
+    random.shuffle(allcwids)
+    for cwid in allcwids:
+        if len(doc4jud) >= judnum:
+            break;
+        if cwid not in doc4jud:
+            doc4jud.append(cwid)
+    doc4test=[cwid for cwid in allcwids if cwid not in doc4jud]
+    return doc4jud, doc4test
+def createJudPairs(doc4jud):
+    return [(doc4jud[i], doc4jud[j]) for i in range(len(doc4jud)) for j in range(i+1, len(doc4jud))]
+def createTestPairs(doc4test, cwidGrad, testnum):
+    abstestpairs=list()
+    tietestpairs=list()
+    testpairs=list()
+    for i in range(len(doc4test)):
+        for j in range(i+1, len(doc4test)):
+            if abs(cwidGrad[doc4test[i]] -  cwidGrad[doc4test[j]])>=2:
+                abstestpairs.append((doc4test[i], doc4test[j]))
+            elif cwidGrad[doc4test[i]] !=  cwidGrad[doc4test[j]]:
+                tietestpairs.append((doc4test[i], doc4test[j]))
+    random.shuffle(abstestpairs)
+    random.shuffle(tietestpairs)
+    for i in range(testnum):
+        if i < len(abstestpairs):
+            testpairs.append(abstestpairs[i])
+        if len(testpairs) >= testnum:
+            break
+        if i < len(tietestpairs):
+            testpairs.append(tietestpairs[i]) 
+        if len(testpairs) >= testnum:
+            break
+    return testpairs
+def createTestDoc4Grad(ldocs, alldocs2consider, testnum):
+    labledocs = dict(ldocs)
+    testdocs=list()
+    grads = list(labledocs.keys())
+    labelidx=dict(zip(grads, [0]*len(grads)))
+    i=len(grads)-1
+    while len(testdocs) < testnum:
+        l = grads[i % len(grads)]
+        i+=1
+        currentidx = labelidx[l]
+        for j in range(currentidx, len(labledocs[l])):
+            doc2consider = labledocs[l][j]
+            if doc2consider in alldocs2consider:
+                testdocs.append(doc2consider)
+                labelidx[l]=j+1
+                break
+    return testdocs
+def writeCFPrefData(qid, query, description, cwiddocid, cwidourls, 
+                    outdir, pair4jud, pair4test):
+    def getPrefLabel(l1, l2):
+        if abs(l1-l2)==2:
+            if l1 > l2:
+                prefl="\"" + "1" + "\""
+            elif l1 < l2:
+                prefl="\"" + "-1" + "\""
+        else:
+            if l1 > l2:
+                prefl="\"" + "1\n0" + "\""
+            elif l1 < l2:
+                prefl="\"" + "-1\n0" + "\""
+            else:
+                prefl="\"" + "0" + "\"" 
+        return prefl
+    def createUrl(docid):
+        url = "http://ttsa.mpi-inf.mpg.de/cwserver/converter?docid="+docid
+        return url
+    def createOurl(cwid):
+        return cwidourls[cwid] if cwid in cwidourls else "-"
+    lines=list()
+    prefgoldreason="Judged by trained editors."
+    headline = ','.join(["lid", "qid", "query","description",
+                    "_golden",
+                    "d1_cwid", "d1_did", "d1_ourl","d1_url", "d1_gjud_trec", 
+                    "d2_cwid", "d2_did", "d2_ourl", "d2_url", "d2_gjud_trec",
+                    "d1_d2_pjud_gold", "d1_d2_pjud_gold_reason"])
+    #lines.append(headline)
+    for docpair in pair4jud:
+        doclist = list(docpair)
+        random.shuffle(doclist)
+        cwid1, cwid2 = docpair
+        lineid = "J" + str(qid) + "%03d"%(len(lines) - 1)
+        line = ','.join([lineid, str(qid), query, description,"False",\
+                cwid1, str(cwiddocid[cwid1]), createOurl(cwid1),\
+                createUrl(cwiddocid[cwid1]),str(cwidGrad[cwid1]),\
+                cwid2, str(cwiddocid[cwid2]), createOurl(cwid2),\
+                createUrl(cwiddocid[cwid2]), str(cwidGrad[cwid2]),\
+                getPrefLabel(cwidGrad[cwid1], cwidGrad[cwid2]), prefgoldreason])
+        lines.append(line)
+    for docpair in pair4test:
+        doclist = list(docpair)
+        random.shuffle(doclist)
+        lineid = "T" + str(qid) + "%03d"%(len(lines) - 1)
+        cwid1, cwid2 = docpair
+        line = ','.join([lineid, str(qid), query, description,"True",\
+                cwid1, str(cwiddocid[cwid1]), createOurl(cwid1),\
+                createUrl(cwiddocid[cwid1]),str(cwidGrad[cwid1]),\
+                cwid2, str(cwiddocid[cwid2]), createOurl(cwid2),\
+                createUrl(cwiddocid[cwid2]), str(cwidGrad[cwid2]),\
+                getPrefLabel(cwidGrad[cwid1], cwidGrad[cwid2]), prefgoldreason])
+        lines.append(line)
+    outdir = outdir + "/pref"
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    outfile=outdir+"/"+str(qid) + ".csv"
+    with open(outfile, "w+") as f:
+        for l in lines:
+            f.write(l + "\n")
+
+def writeCFGradedData(qid, query, description, 
+                      cwiddocid, cwidourls, outdir,
+                      doc4jud, doc4test):
+    def createGold(grade):
+        if grade == 1 or grade == 2:
+            gold = "\"" + "1\n2" + "\""
+        else:
+            gold =  "\"" + str(grade) + "\""
+        return gold
+    def createUrl(docid):
+        url = "http://ttsa.mpi-inf.mpg.de/cwserver/converter?docid="+docid
+        return url
+            
+    lines=list()
+    gradgoldreason="Judged by trained editors"
+    headline = ','.join(["lid", "qid", "query","description",
+                    "_golden",
+                    "d_cwid", "d_did", "d_ourl", "d_url",
+                        "d_gjud_gold","d_gjud_gold_reason"])
+    #lines.append(headline)
+    for cwid in doc4jud:
+        lineid = "J" + str(qid) + "%03d"%(len(lines) - 1)
+        line = ','.join([lineid, str(qid), query, description,"False",\
+                cwid, str(cwiddocid[cwid]), cwidourls[cwid],createUrl(cwiddocid[cwid]),\
+                createGold(cwidGrad[cwid]),gradgoldreason])
+        lines.append(line)
+
+    for cwid in doc4test:
+        lineid = "T" + str(qid) + "%03d"%(len(lines) - 1)
+        line = ','.join([lineid, str(qid), query, description,\
+                        "True", cwid, str(cwiddocid[cwid]), \
+                        cwidourls[cwid],createUrl(cwiddocid[cwid]),\
+                        createGold(cwidGrad[cwid]),gradgoldreason])
+        lines.append(line)
+    outdir = outdir + "/grad"
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    outfile=outdir+"/"+str(qid) + ".csv"
+    with open(outfile, "w+") as f:
+        for l in lines:
+            f.write(l + "\n")
+
+
+
+
+conf = (SparkConf()
+        .setAppName("empirical_study")
+        .set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+        .set("spark.local.dir","/GW/D5data-2/khui/cw-docvector-termdf/tmp")
+        .set("spark.kryoserializer.buffer.mb","128")
+        .set("spark.driver.maxResultSize","2g"))
+sc = SparkContext(conf = conf)
+
+dictfile="/GW/D5data-2/khui/Clueweb12/html/cw4y/dictionary/did2docurl.dict"
+urlfile="/user/khui/data/ExtractCwdocs/cwidurl/cw4y"
+queryfile="/user/khui/data/query/wtff.xml"
+outdir="/GW/D5data-2/khui/EmpiricalStudy/crowdFlower/pairdata"
+
+
+qidyear=dict(zip(list(range(101, 301)), ['wt11']*50+['wt12']*50+['wt13']*50+['wt14']*50))
+dictfile="/GW/D5data-2/khui/Clueweb12/html/cw4y/dictionary/did2docurl.dict"
+qidCwiddocid = readDid4ttsa(dictfile)
+queryfile="/user/khui/data/query/wtff.xml"
+pairperpage=2.0
+docperpage=4.0
+qidQuery=readInQuery(queryfile)
+
+qids=list(range(251,301)) + list(range(201,251))
+for qid in qids:
+    year=qidyear[qid]
+    qrelf="/user/khui/data/qrel/qrels.adhoc." + year
+    qtype, query, descrip = qidQuery[qid]
+    cwidGrad=relgradedqrel(sc, qid, qrelf, parNum=24)
+    ldocs = labelDocs(cwidGrad)
+    cwidurls = readcwidurl(sc, qid, urlfile, parNum=24)
+    doc4jud, remaindocs = selectDocs(ldocs, cwidGrad, judnum=12)
+    pair4jud=createJudPairs(doc4jud)
+    ptestnum = int(max(math.ceil(len(pair4jud)/pairperpage) + pairperpage, pairperpage+3))
+    gtestnum = int(max(math.ceil(len(doc4jud)/docperpage) + docperpage, docperpage+3))
+    pair4test=createTestPairs(remaindocs, cwidGrad, testnum=ptestnum)
+    doc4test = createTestDoc4Grad(ldocs, remaindocs, testnum=gtestnum)    
+    writeCFPrefData(qid, query, descrip, qidCwiddocid[qid], cwidurls, outdir, pair4jud, pair4test)   
+    writeCFGradedData(qid, query, descrip, qidCwiddocid[qid], cwidurls, outdir,  doc4jud, doc4test)    
+    print(' '.join(["INFO",str(qid), 'dnum4j:%d'%len(doc4jud), 'dnum4t:%d'%len(doc4test),\
+                'ptnum4j:%d'%len(pair4jud), 'ptnum4t:%d'%len(pair4test)]))
---
+++ src/main/resources/python/pairdata4cf.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+timestamp=`date +"%m%d%M%H"`
+id=$timestamp.`hostname`.$BASHPID
+echo $id
+
+javaworkspace=/home/khui/workspace/IdeaProjects/docsimilarity/target
+jdk=/home/khui/workspace/javaworkspace/java-8-sun/jdk1.8.0_45
+log4jconf=/home/khui/workspace/javaworkspace/log4j.xml
+tmpdir=/GW/D5data-2/khui/cw-docvector-termdf/tmp
+
+source /home/khui/workspace/pyenv/startpy27.sh
+pythondir="/home/khui/workspace/IdeaProjects/docsimilarity/src/main/resources/python/ecir17"
+
+START=$(date +%s.%N)
+islocal=true
+if ! $islocal
+then
+	echo run on the cluster
+	# for cluster
+	spark-submit \
+	--master yarn://139.19.52.105 \
+	--driver-memory 20G \
+	--executor-memory 40G \
+	--executor-cores 8 \
+	--num-executors 4 \
+	--driver-java-options "-Djava.io.tmpdir=$tmpdir" \
+	$pythondir/pairdata4cf.py
+else
+	echo run locally
+	spark-submit \
+	--master local[*] \
+	--driver-memory 55G \
+	--driver-java-options "-Djava.io.tmpdir=$tmpdir" \
+	$pythondir/pairdata4cf.py
+fi
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo $expid finished within $DIFF
+
+
